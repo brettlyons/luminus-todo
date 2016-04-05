23966ad080c74fec020d358d33d7e588e21df11b
Brett Lyons <blyons@students.naropa.edu>
2016-04-05 12:56:05 -0600

    Complete step 3.  /about endpoint added.

--- src/clj/luminus_todo/routes/home.clj
+++ src/clj/luminus_todo/routes/home.clj
@@ -5,9 +5,14 @@
             [clojure.java.io :as io]))
 
 (defn home-page []
+  ; (layout/render "home.html")
   (str "<img src='http://www.lfed-mw.com/animations/under_construction_animated.gif'>"))
+
+(defn about-page []
+  (str "<h1>This project is a todo list implementation.  For DemocracyWorks!"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (GET "/about" [] (about-page))
   (GET "/docs" [] (response/ok (-> "docs/docs.md" io/resource slurp))))
 
